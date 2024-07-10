d6153c8de93bb1ed8ee02c61325d126e7d5027a9
sshahary <145033155+sshahary@users.noreply.github.com>
2024-07-10 21:02:32 +0200

    see later ex05

---
+++ ex05/Harl.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 21:00:25 by sshahary          #+#    #+#             */
+/*   Updated: 2024/07/10 21:00:46 by sshahary         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl {
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+public:
+    void complain(std::string level);
+};
+
+#endif
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,20 @@
 /*   By: sshahary <sshahary@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 20:53:29 by sshahary          #+#    #+#             */
-/*   Updated: 2024/07/10 20:53:30 by sshahary         ###   ########.fr       */
+/*   Updated: 2024/07/10 21:01:41 by sshahary         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main() {
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("INFO");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("UNKNOWN");
+
+    return 0;
+}
